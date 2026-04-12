e6a758df1fc71bfce7f00289fa25c675abbee768
ten10second <shizm0126@gmail.com>
2026-04-12 21:41:57 +0800

    IP-Adapter style

--- models/unet/__init__.py
+++ models/unet/__init__.py
@@ -3,6 +3,7 @@ UNet-related modules.
 """
 
 from .continuous_xy_georope import ContinuousXYGeoRoPE
+from .decoupled_sat_cross_attn import DecoupledSatCrossAttn
 from .gated_residual_inject import GatedResidualInject
 from .relative_position_attention import RelativePositionAttention
 from .satellite_reading_attention import SatelliteReadingAttention
