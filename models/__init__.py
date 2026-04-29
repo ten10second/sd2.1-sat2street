e27ac31c0d2d76d2c32b9085176cc17d3e80b0ed
shizhm <shizm0126@gmail.com>
2026-04-29 18:23:23 +0800

    delate SatelliteReadingAttention

--- models/__init__.py
+++ models/__init__.py
@@ -7,8 +7,4 @@ from .sd_trainer import create_sd_model, SatelliteConditionedSDModel, SDTrainer
 from .encoders.satellite_condition_encoder import SatelliteConditionEncoder
 from .encoders.relative_coordinate_encoder import RelativeCoordinateEncoder
 from .unet.relative_position_attention import RelativePositionAttention
-from .unet.continuous_xy_georope import ContinuousXYGeoRoPE
-from .unet.satellite_reading_attention import SatelliteReadingAttention
-from .unet.gated_residual_inject import GatedResidualInject
-from .unet.satellite_reading_block import SatelliteReadingBlock
 from .unet.geometric_transport_reading import GeometricTransportReadingBlock
--- models/unet/__init__.py
+++ models/unet/__init__.py
@@ -2,9 +2,5 @@
 UNet-related modules.
 """
 
-from .continuous_xy_georope import ContinuousXYGeoRoPE
-from .gated_residual_inject import GatedResidualInject
 from .relative_position_attention import RelativePositionAttention
-from .satellite_reading_attention import SatelliteReadingAttention
-from .satellite_reading_block import SatelliteReadingBlock
 from .geometric_transport_reading import GeometricTransportReadingBlock
